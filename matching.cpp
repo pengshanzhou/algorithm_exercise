b5d0306ef9b6784141f37170e59c844e1654d00e
PengshanZhou <zhoupengshan@126.com>
2014-03-11 13:48:37 +0800

    简单的字符正则匹配的实现
    简单的正则匹配，str 为表达字符，dest为匹配字符； '*'匹配任意非空字符串，'?'匹配任意字符；如str="a*"匹配任意以a开头的字符串如dest="abcd".

---
+++ matching.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+using namespace std;
+bool ismatch(char *src, char *dest)
+{
+	if(src==NULL || dest == NULL )
+	return 0;
+	int len1=strlen(src);
+	int len2=strlen(dest);	
+	if(len1>len2)
+	return 0;
+	if(len1==0 && len2==0)
+	return 0;
+	while(*src!=0 && *dest!=0 )
+	{
+		if(*src==*dest || *src=='?')
+		{
+			++src;
+			++dest;
+		}
+		else if(*src=='*')
+		{
+			++src;
+			while(*dest!= 0)
+			{
+			  if(ismatch(src,dest++))
+		      return 1;
+			}
+            break;
+		}
+		else if(*src != *dest )
+		return 0;
+	}
+	if(*src==0 && *dest==0)
+       return 1;
+    else return 0;
+}	
+int main()
+{
+	char * str[5]={"*","abc","??b","a*b","cd"};
+	char * dest[5]={"ahjjk","abc","ssb","asddddb","cc"};
+	for(int i=0;i<5;i++)
+	{
+		cout<<ismatch(str[i],dest[i])<<endl;;	
+	}
+}
